484dccf91cc25e91b6a62433fdf6cb6d3dff9702
Manuel Coenen <manuel@duet3d.com>
2020-11-03 12:55:45 +0100

    Compiling version of PanelDue flash code (needs more work)

--- src/Networking/FirmwareUpdater.cpp
+++ src/Networking/FirmwareUpdater.cpp
@@ -11,6 +11,10 @@
 #include "Network.h"
 #include "Platform.h"
 #include "RepRap.h"
+#include "bossa/Samba.h"
+#include "bossa/Device.h"
+#include "bossa/Flasher.h"
+#include "bossa/SerialPort.h"
 
 #if HAS_WIFI_NETWORKING
 #include "ESP8266WiFi/WifiFirmwareUploader.h"
@@ -21,6 +25,57 @@ namespace FirmwareUpdater
 	const unsigned int WifiFirmwareModule = 1;
 	// Module 2 used to be the DWC binary file but is no longer used
 	const unsigned int WifiExternalFirmwareModule = 3;
+
+	const unsigned int PanelDueFirmwareModule = 4;
+
+	class AuxSerialPort : public SerialPort
+	{
+	public:
+		AuxSerialPort(UARTClass *uartClass) : uart(uartClass), autoflush(false)  {}
+	    ~AuxSerialPort() {}
+
+	    bool open(int baud = 115200,
+	              int data = 8,
+	              SerialPort::Parity parity = SerialPort::ParityNone,
+	              SerialPort::StopBit stop = SerialPort::StopBitOne) { return true; }
+	    void close() {}
+
+	    bool isUsb() { return false; };
+
+	    int read(uint8_t* data, int size);
+	    int write(const uint8_t* data, int size);
+	    int get() { return this->uart->read(); }
+	    int put(int c) { return this->uart->write(c); }
+
+	    bool timeout(int millisecs) { return true; }
+	    void flush() { this->uart->flush(); }
+	    void setDTR(bool dtr) {}
+	    void setRTS(bool rts) {}
+	    void setAutoFlush(bool newAutoflush) { this->autoflush = newAutoflush; }
+	private:
+		UARTClass *uart;
+		bool autoflush;
+	};
+
+	int AuxSerialPort::read(uint8_t* data, int size) {
+		int res = 0;
+    	for (int i = 0; i < size; ++i)
+    	{
+    		*data = get();
+    		++data;
+    		++res;
+    	}
+        return res;
+    }
+
+	int AuxSerialPort::write(const uint8_t* data, int size) {
+    	auto res = this->uart->write(data, size);
+        if (autoflush)
+        {
+            flush();
+        }
+        return res;
+    }
 
 	// Check that the prerequisites are satisfied.
 	// Return true if yes, else print a message and return false.
@@ -78,6 +133,48 @@ namespace FirmwareUpdater
 		}
 # endif
 #endif
+
+		// FIXME: This needs to be rewritten as a separate updater with a state-machine
+		if (module == PanelDueFirmwareModule)
+		{
+			try
+			{
+				Samba samba;
+
+				AuxSerialPort port(&SERIAL_AUX_DEVICE);
+				samba.connect(&port);
+
+				Device device(samba);
+				device.create();
+
+				Flash* flash = device.getFlash();
+
+				Flasher flasher(samba, device);
+
+				flasher.lock(false);
+				flasher.erase(0);
+				flasher.write(PANEL_DUE_FIRMWARE_FILE, DEFAULT_SYS_DIR, 0);
+
+				uint32_t pageErrors;
+				uint32_t totalErrors;
+				if (!flasher.verify(PANEL_DUE_FIRMWARE_FILE, DEFAULT_SYS_DIR, pageErrors, totalErrors, 0))
+				{
+					MessageType mt = AddError(MessageType::GenericMessage);
+					reprap.GetPlatform().MessageF(mt, "Verify failed: Page errors: %" PRIu32 " - Byte errors: %" PRIu32 "", pageErrors, totalErrors);
+					return;
+				}
+
+				flash->writeOptions();
+				device.reset();
+			}
+			catch (GCodeException& ex)
+			{
+				MessageType mt = AddError(MessageType::GenericMessage);
+				String<StringLength100> errorMessage;
+				ex.GetMessage(errorMessage.GetRef(), nullptr);
+				reprap.GetPlatform().MessageF(mt, "%s", errorMessage.c_str());
+			}
+		}
 	}
 }
 
